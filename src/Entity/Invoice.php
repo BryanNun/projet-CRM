09b4a6dd229b703fc2fa6d9688b72962c840aec0
BryanNun <nuninger.bryan@gmail.com>
2020-04-03 18:41:18 +0200

    Added subresources

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -12,6 +12,11 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\InvoiceRepository")
  * @ApiResource(
+ *  subresourceOperations={
+ *      "api_customers_invoices_get_subresource"={
+ *          "normalization_context"={"groups"={"invoices_subresource"}}
+ *      }
+ * },
  *  attributes={
  *      "pagination_enabled"=true,
  *      "pagination_items_per_page"=20,
@@ -27,25 +32,25 @@ class Invoice
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"invoices_read", "customers_read"})
+     * @Groups({"invoices_read", "customers_read", "invoices_subresources"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="float")
-     * @Groups({"invoices_read", "customers_read"})
+     * @Groups({"invoices_read", "customers_read", "invoices_subresources"})
      */
     private $amount;
 
     /**
      * @ORM\Column(type="datetime")
-     * @Groups({"invoices_read", "customers_read"})
+     * @Groups({"invoices_read", "customers_read", "invoices_subresources"})
      */
     private $sentAt;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"invoices_read", "customers_read"})
+     * @Groups({"invoices_read", "customers_read, "invoices_subresources""})
      */
     private $status;
 
@@ -58,13 +63,13 @@ class Invoice
 
     /**
      * @ORM\Column(type="integer")
-     * @Groups({"invoices_read", "customers_read"})
+     * @Groups({"invoices_read", "customers_read, "invoices_subresources""})
      */
     private $chrono;
 
     /**
      * Permet de récupérer le User affilié à la facture
-     * @Groups({"invoices_read"})
+     * @Groups({"invoices_read, "invoices_subresources""})
      * @return User
      */
     public function getUser() : User {
