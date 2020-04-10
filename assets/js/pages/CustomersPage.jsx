de289e322e8fc9790a9850783e0d0f216aed1574
BryanNun <nuninger.bryan@gmail.com>
2020-04-10 13:26:46 +0200

    Added filtre

--- assets/js/pages/CustomersPage.jsx
+++ assets/js/pages/CustomersPage.jsx
@@ -6,6 +6,7 @@ const CustomersPage = (props) => {
 
     const [customers, setCustomers] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         axios
@@ -31,14 +32,38 @@ const CustomersPage = (props) => {
 
     const handlePageChange = (page) => {
         setCurrentPage(page);
-    }
+    };
+
+    const handleSearch = event => {
+        const value = event.currentTarget.value;
+        setSearch(value);
+        setCurrentPage(1);
+    };
 
     const itemsPerPage = 10;
-    const paginatedCustomers = Pagination.getData(customers, currentPage, itemsPerPage);
+
+    const filteredCustomers = customers.filter(
+        c =>
+            c.firstName.toLowerCase().includes(search.toLowerCase()) ||
+            c.lastName.toLowerCase().includes(search.toLowerCase()) ||
+            c.email.toLowerCase().includes(search.toLowerCase()) ||
+            (c.company && c.company.toLowerCase().includes(search.toLowerCase()))
+
+    );
+
+    const paginatedCustomers = Pagination.getData(
+        filteredCustomers,
+        currentPage,
+        itemsPerPage
+    );
 
     return (
         <>
             <h1>List des clients</h1>
+
+            <div className="form-group">
+                <input type="text" onChange={handleSearch} value={search} className="form-control" placeholder="Rechercher ..." />
+            </div>
 
             <table className="table table-hover">
                 <thead>
@@ -78,11 +103,14 @@ const CustomersPage = (props) => {
                         </tr>))}
                 </tbody>
             </table>
-            <Pagination currentPage={currentPage}
-                itemsPerPage={itemsPerPage}
-                length={customers.length}
-                onPageChanged={handlePageChange}
-            />
+
+            {itemsPerPage < filteredCustomers.length && (
+                <Pagination currentPage={currentPage}
+                    itemsPerPage={itemsPerPage}
+                    length={filteredCustomers.length}
+                    onPageChanged={handlePageChange}
+                />
+            )}
         </>
     );
 }
