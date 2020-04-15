1bb5baed7f75db342830508b440665c015d63bc3
BryanNun <nuninger.bryan@gmail.com>
2020-04-15 11:24:08 +0200

    Edit home page

--- assets/js/pages/HomePage.jsx
+++ assets/js/pages/HomePage.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const HomePage = (props) => {
     return (<div className="jumbotron">
-        <h1 className="display-3">Hello, world!</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <h1 className="display-3">Bienvenue sur NB Gestion</h1>
+        <p className="lead">NB Gestion est un site gratuit qui vous permet de gérer votre portefeuille client</p>
         <hr className="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p>Il vous sera possible après inscription, de créer des clients et leurs factures, suivre le statut des factures et avoir un historique des toutes vos ventes. </p>
         <p className="lead">
             <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </p>
